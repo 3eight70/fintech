1f7f070feb9bd149a9c5e1a75bdf9ef524c49fd2
Danil Vasilev <gbhfns90@gmail.com>
2024-08-30 21:07:06 +0700

    Созданы методы для обработки json файлов из папки resources

---
+++ src/test/java/ru/fintech/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fintech.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
