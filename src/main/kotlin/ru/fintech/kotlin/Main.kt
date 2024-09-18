ab0da188477f5774bada3d45d3772d8bf715f2c8
Danil Vasilev <gbhfns90@gmail.com>
2024-09-18 10:30:47 +0700

    Добавлен html dsl printer

--- src/main/kotlin/ru/fintech/kotlin/Main.kt
+++ src/main/kotlin/ru/fintech/kotlin/Main.kt
@@ -4,15 +4,19 @@ import ru.fintech.kotlin.utils.NewsSaver
 import ru.fintech.kotlin.service.NewsService
 import ru.fintech.kotlin.service.NewsService.getMostRatedNews
 import ru.fintech.kotlin.utils.formatNews
+import ru.fintech.kotlin.utils.renderNewsPage
 import java.time.LocalDate
 
 suspend fun main() {
     System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug")
     val news = NewsService.getNews(count = 10)
     val mostRatedNews = news.getMostRatedNews(5,
-        LocalDate.of(2024, 8, 1)..LocalDate.of(2024, 9, 14))
+        LocalDate.of(2024, 8, 1)..LocalDate.now())
     //NewsSaver.saveNews("""D:\Programming\asf.csv""", mostRatedNews)
-
-    val formattedNews = formatNews(mostRatedNews)
-    println(formattedNews)
+    println("Новости в формате markdown")
+    val markdownNews = formatNews(mostRatedNews)
+    println(markdownNews)
+    println("Новости в формате html")
+    val htmlNews = renderNewsPage(mostRatedNews)
+    println(htmlNews)
 }
